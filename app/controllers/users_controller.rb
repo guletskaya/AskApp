980bde08cbfd963bbb81fdf3cd4d52a931b84c5c
guletskaya <guletskayaliza@gmail.com>
2022-02-06 13:46:35 +0300

    Add log in action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
     def new
       @user = User.new  
     end
 
     def show
-        @user = User.find(params[:id])
+        
         @questions = @user.questions
     end
 
@@ -16,6 +18,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the AskAll #{@user.username}, you have successfully signed up!"
             redirect_to @user
         else
@@ -24,11 +27,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+        
     end
 
     def update
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "#{@user.username}, you have successfully updated your profile!"
             redirect_to @user
@@ -41,5 +44,9 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
